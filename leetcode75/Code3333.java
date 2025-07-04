968e13662ef531c4420239da9dae109663ef4579
yanghm55 <yanghm55@mideqa.com>
2025-07-04 09:43:02 +0800

    book and leetcode75

--- leetcode75/Code3333.java
+++ leetcode75/Code3333.java
@@ -1,8 +1,12 @@
 package leetcode75;
 
+import java.util.HashMap;
+
 public class Code3333 {
     /**
      * 暴力解法
+     * <p>
+     * 题目给定word的长度5*10^5
      *
      * @param word
      * @param k
@@ -14,10 +18,14 @@ public class Code3333 {
         }
         if (k >= word.length()) return 1;
         char[] str = word.toCharArray();
-        return process(str, 0, k, 0);
+        HashMap<String, Integer> map = new HashMap<>();
+        return process(str, 0, k, 0, map);
     }
 
-    public int process(char[] str, int index, int k, int count) {
+    public int process(char[] str, int index, int k, int count, HashMap<String, Integer> map) {
+        if (map.containsKey(index + "-" + count)) {
+            return map.get(index + "-" + count);
+        }
         if (index == str.length) {
             return count < k ? Integer.MAX_VALUE : 1;
         } else {
@@ -33,11 +41,12 @@ public class Code3333 {
             for (int j = 1; j <= (i - index); j++) {
                 // 当前字符选多少个
                 // 下一个位置是i
-                int next = process(str, i, k, count + j);
+                int next = process(str, i, k, count + j, map);
                 if (next != Integer.MAX_VALUE) {
-                    ans += next;
+                    ans = (ans + next % 1000000007) % 1000000007;
                 }
             }
+            map.put(index + "-" + count, ans % 1000000007);
             return ans;
         }
     }
